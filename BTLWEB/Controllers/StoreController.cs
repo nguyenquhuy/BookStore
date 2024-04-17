94faca2e653b1891797404cefe65c195af1d5895
huy <huy@mail>
2024-04-17 08:58:24 +0700

    pagination

--- BTLWEB/Controllers/StoreController.cs
+++ BTLWEB/Controllers/StoreController.cs
@@ -15,8 +15,10 @@ namespace BTLWEB.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index(string searchString, string minPrice, string maxPrice)
+        public async Task<IActionResult> Index(string searchString, int page = 1)
         {
+            int pageSize = 4; // Set the page size to 4 items per page
+
             var books = _context.Book.Select(b => b);
 
             if (!string.IsNullOrEmpty(searchString))
@@ -24,8 +26,17 @@ namespace BTLWEB.Controllers
                 books = books.Where(b => b.Title.Contains(searchString) || b.Author.Contains(searchString));
             }
 
+            var totalCount = await books.CountAsync();
+            var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+
+            books = books.Skip((page - 1) * pageSize).Take(pageSize);
+
+            ViewBag.TotalPages = totalPages;
+            ViewBag.CurrentPage = page;
+
             return View(await books.ToListAsync());
         }
+
 
         public async Task<IActionResult> Details(int? id)
         {
--- BTLWEB/obj/Debug/net8.0/BTLWEB.AssemblyInfo.cs
+++ BTLWEB/obj/Debug/net8.0/BTLWEB.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTLWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0078fdc35e51bc5c3638f330e0f7cf1b7b7578d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d54e1788135db189c2bcbbfb962ac28a8951a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTLWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTLWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
